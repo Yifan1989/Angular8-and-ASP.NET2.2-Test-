9dedc376ede4d78dbbb726750af90d9eaef82b4d
Yifan1989 <j.codes1989@gmail.com>
2019-07-02 20:16:40 +1200

    entity

---
+++ Test_Full/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Full.Models;
+
+namespace Test_Full.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+    }
+}
